a8b54a7abaa6b32ab199a253c66131d2b262b9b3
zhengY <3289218653@qq.com>
2021-04-24 23:28:27 +0800

    挖矿数据 test

--- test-code/load_kaggle_mal.py
+++ test-code/load_kaggle_mal.py
@@ -1,15 +1,16 @@
 # coding: utf-8
-import os
-import time
-import sys
-import yaml
+import glob
 import numpy as np
 import pandas as pd
-from ..src.util import ExeDataset,write_pred
-from ..src.model import MalConv
-from torch.utils.data import DataLoader
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.autograd import Variable
 
+def get_data_label(data_path, label_values, label_path):
+    names = []
+    df = pd.DataFrame()
+    for path in glob.glob(data_path):
+        names.append(path.split('/')[-1])
+
+    df["id"] = names
+    df["labels"] = label_values
+    df.to_csv(label_path + "/black.csv", index=False, encoding="utf-8")
+
+get_data_label("/Users/apple/Desktop/机器学习/DataCon_2020/恶意代码检测/gray/1_2000_black/*", 1, "/Users/apple/Documents/GitHub/Deep learning for malware detection/MalConv-Pytorch/data")
