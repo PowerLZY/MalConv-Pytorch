577bd85e0608ec14f185122fa6056f74300bca19
zhengY <3289218653@qq.com>
2021-05-05 22:31:40 +0800

    保存模型到 checkpoint

--- malconv-mining.py
+++ malconv-mining.py
@@ -29,9 +29,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 use_gpu = True                #
 use_cpu = 32                # Number of cores to use for data loader
 display_step = 10           # Std output update rate during training
-#test_step = 20             # Test per n step
+test_step = 150             # Test per n step
 learning_rate = 0.0001     #
-max_step = 2000            # Number of steps to train
+max_step = 1000            # Number of steps to train
 batch_size = 32          #
 first_n_byte = 2000000     # First N bytes of a PE file as the input of MalConv (defualt: 2 million)
 window_size = 500          # Kernel size & stride for Malconv (defualt : 500)
@@ -40,9 +40,8 @@ log_dir = curPath + '/log/'
 pred_dir = curPath + '/pred/'
 checkpoint_dir = curPath +'/checkpoint/'
 log_file_path = log_dir+exp_name+'.log'
-chkpt_acc_path = checkpoint_dir+exp_name+'.model'
+chkpt_acc_path = checkpoint_dir+exp_name+'.pt'
 pred_path = pred_dir+exp_name+'.pred'
-
 
 #get_data_label("/Users/apple/Desktop/机器学习/DataCon_2020/恶意代码检测/gray/1_2000_black/*", label_path, "/black.csv")
 #get_data_label("/Users/apple/Desktop/机器学习/DataCon_2020/恶意代码检测/gray/1_4000_white/*", label_path, "/white.csv")
@@ -94,7 +93,7 @@ log.write('step,tr_loss, tr_acc, val_loss, val_acc, time\n')
 valid_best_acc = 0.0
 total_step = 0
 step_cost_time = 0
-valid_idx = list(validset.index)
+valid_idx = list(validset['id'])
 
 while total_step < max_step:
 
@@ -118,28 +117,26 @@ while total_step < max_step:
         adam_optim.step()
 
         history['tr_loss'].append(loss.cpu().data.numpy())
-        history['tr_acc'].extend(
-            list(label.cpu().data.numpy().astype(int) == (sigmoid(pred).cpu().data.numpy() + 0.5).astype(int)))
+        history['tr_acc'].extend(list(label.cpu().data.numpy().astype(int) == (sigmoid(pred).cpu().data.numpy() + 0.5).astype(int)))
 
         step_cost_time = time.time() - start
 
         if (step + 1) % display_step == 0:
             print(step_msg.format(total_step, np.mean(history['tr_loss']), np.mean(history['tr_acc']), step_cost_time))
 
-
         total_step += 1
 
         # Interupt for validation
-        #if total_step % test_step == 0:
-            #break
+        if total_step % test_step == 0:
+            break
 
     # Testing
     history['val_loss'] = []
     history['val_acc'] = []
     history['val_pred'] = []
 
-
     for _, val_batch_data in enumerate(validloader):
+
         cur_batch_size = val_batch_data[0].size(0)
 
         exe_input = val_batch_data[0].cuda() if use_gpu else val_batch_data[0]
@@ -162,4 +159,15 @@ while total_step < max_step:
     print(valid_msg.format(total_step, np.mean(history['tr_loss']), np.mean(history['tr_acc']),
                            np.mean(history['val_loss']), np.mean(history['val_acc'])))
 
+    # 保存模型
+    if valid_best_acc < np.mean(history['val_acc']):
+        valid_best_acc = np.mean(history['val_acc'])
+        torch.save(malconv.state_dict(), chkpt_acc_path)
+        # model_dict=model.load_state_dict(torch.load(PATH))
+        print('Checkpoint saved at', chkpt_acc_path)
+        write_pred(history['val_pred'], valid_idx, pred_path)
+        print('Prediction saved at', pred_path)
+
+    history['tr_loss'] = []
+    history['tr_acc'] = []
 
