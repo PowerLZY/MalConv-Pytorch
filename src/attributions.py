44c4b2ad85b76eb4ca0c16a684604561570d2de6
zhengY <3289218653@qq.com>
2021-05-11 17:36:15 +0800

    完善样本的贡献度热力图和直方图显示流程;添加多组展示结果

--- src/attributions.py
+++ src/attributions.py
@@ -7,7 +7,7 @@ sys.path.append(rootPath)
 import numpy as np
 import torch
 from torch.autograd import Variable
-from src.model import MalConv
+from src.model import MalConv,PreMalConv
 from src.util import *
 from torch.utils.data import DataLoader
 
@@ -15,66 +15,40 @@ from captum.attr import (
     IntegratedGradients,# 积分梯度
     LayerIntegratedGradients # embedding层积分梯度
 )
-label_path = rootPath + "/data/"
+
 train_data_path =rootPath + "/data/one/"  # Training data
-train_label_path = rootPath +'/data/example-train-label.csv' # Training label
-valid_label_path = rootPath +'/data/example-valid-label.csv' # Validation Label
-
-exp_name = 'IG'
-
-### Parameter
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+model_path = rootPath + '/checkpoint/pretrained_malconv.pth'
+save_path = rootPath + "/picture"
+filename = get_filename(train_data_path+"*") # 样本列表
+labels = [1 for _ in filename] # filename 标签列表
+# Parameter
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 use_gpu = True              #
 use_cpu = 1                # Number of cores to use for data loader
 batch_size = 1          #
-first_n_byte = 2000000     # First N bytes of a PE file as the input of MalConv (defualt: 2 million)
+first_n_byte = 2 ** 20     # First N bytes of a PE file as the input of MalConv (defualt: 2 million)
 
-### output path
-log_dir = curPath + '/log/'
-pred_dir = curPath + '/pred/'
-checkpoint_dir = curPath +'/checkpoint/'
-log_file_path = log_dir+exp_name+'.log'
-chkpt_acc_path = checkpoint_dir+exp_name+'.pt'
-pred_path = pred_dir+exp_name+'.pred'
-"""
-df1 = pd.read_csv(label_path+'white.csv')
-df2 = pd.read_csv(label_path+'black.csv')
-dataset = merge(df1, df2, "dataset", label_path)
-
-train, valid, train_label, valid_label = train_test_split(
-    dataset['id'],
-    dataset['labels'],
-    test_size=0.2,
-    stratify= dataset['labels'],
-    random_state=100)
-
-trainset = pd.DataFrame({'id':train, 'labels':train_label})
-validset = pd.DataFrame({'id':valid, 'labels':valid_label})
-
-trainset.to_csv(label_path + "example-train-label.csv", index=False, header= False, encoding="utf-8")
-validset.to_csv(label_path + "example-valid-label.csv", index=False, header= False, encoding="utf-8")
-"""
-model = MalConv()
-model.load_state_dict(torch.load('/home/lizy/ml/MalConv-Pytorch/checkpoint/malconv.pt'))
+# 加载模型
+model = PreMalConv()
+model.load_state_dict(torch.load(model_path))
 model.eval()
-
-torch.manual_seed(123)
-np.random.seed(123)
-# 文件名列表、数据文件夹、文件列表标签、前n位字节
-validloader = DataLoader(ExeDataset(['Backdoor.Win32.Agent.bflv_ce22.exe'], train_data_path, [1],first_n_byte),
-                        batch_size=batch_size, shuffle=False, num_workers=use_cpu)
 if use_gpu:
     model = model.cuda()
 
-for _, val_batch_data in enumerate(validloader):
+# 文件名列表、数据文件夹、文件列表标签、前n位字节
+validloader = DataLoader(ExeDataset(filename, train_data_path, labels, first_n_byte),
+                        batch_size=batch_size, shuffle=False, num_workers=use_cpu)
+
+# 显示 train_data_path 下的样本的贡献度热力图和直方图
+for x, val_batch_data in enumerate(validloader):
     cur_batch_size = val_batch_data[0].size(0)
 
     exe_input = val_batch_data[0].cuda() if use_gpu else val_batch_data[0]
     exe_input = Variable(exe_input.long(), requires_grad=False)
-    baseline = torch.zeros(1, 2000000).cuda() if use_gpu else torch.zeros(1, 2000000)
+    baseline = torch.zeros(1, first_n_byte).cuda() if use_gpu else torch.zeros(1, first_n_byte)
     baseline = Variable(baseline.long(), requires_grad=False)
 
-    lig = LayerIntegratedGradients(model, model.embed)
+    lig = LayerIntegratedGradients(model, model.embedding_1) # MalConv：model.embed; PreMalConv: model.embedding_1
     attributions_ig = lig.attribute(exe_input, baseline, target=0)
 
     attributions = attributions_ig.sum(dim=2).squeeze(0)
@@ -84,9 +58,11 @@ for _, val_batch_data in enumerate(validloader):
     print('IG Attributions:', attributions)
     malware = exe_input.squeeze(0).cpu().detach().numpy()
     conf = model(exe_input)
-    title = 'Confidence: {0:.4f}%\nDOS + COFF + OPT + SECT Headers\nBaseline : empty file'.format(conf.item() * 100)
-    plot_code_segment(malware, 0, 512, attributions, title, force_plot=True, show_positives=True, show_negatives=True)
+    title = 'Confidence: {0:.4f}%\nDOS + COFF + OPT + SECT Headers\nBaseline : empty file'.format(conf.item())
+    plot_code_segment(malware, 0, 512, attributions, title, force_plot=True,
+                      show_positives=True, show_negatives=True, save_path=save_path, filename = filename[x])
     # 字节流转化
     malware = [i-1 for i in malware.tolist() if i > 0]
-    plot_header_contribution_histogram(bytearray(malware), attributions, force_plot=True)
+    plot_header_contribution_histogram(bytearray(malware), attributions, force_plot=True,
+                                       save_path = save_path, filename = filename[x])
     break;
